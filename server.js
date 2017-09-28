197b510e81b8d0ca66163341e8afc9679166e480
ulriktestmann <ulrik.testmann@gmail.com>
2017-09-28 22:38:24 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser")
+
+var app = express();
+var path = __dirname + "/public/";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res){
+	res.sendFile("index.html")
+});
+
+app.get("/contact", function(req, res){
+	res.sendFile(path + "contact.html")
+})
+
+app.get("/about", function (req, res){
+	res.sendFile(path + "about.html")
+});
+app.get("/pages", function(req, res){
+	res.sendFile(path + "pages.html")
+})
+
+
+
+
+
+app.listen(12000)
